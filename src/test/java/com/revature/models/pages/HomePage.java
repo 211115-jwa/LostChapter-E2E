fb117250b1f3af9f4dd1a30944e03c872c60be40
ricky23i <rduenas2@illinois.edu>
2022-02-11 09:53:19 -0600

    pickles

---
+++ src/test/java/com/revature/models/pages/HomePage.java
@@ -0,0 +1,34 @@
+package com.revature.models.pages;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+import com.revature.models.components.FrontPageComponent;
+import com.revature.models.components.NavbarComponent;
+import com.revature.models.components.ProductModale;
+
+public class HomePage {
+	
+private  WebDriver driver;
+
+//	@FindBy(id="featuredbooks")
+//	private WebElement featureDisplay;
+//	
+//	@FindBy(id="booktitlegenre")
+//	private WebElement genreDisplay;
+//	
+//	@FindBy(id="salebooks")
+//	private WebElement saleDisplay;
+	
+	public HomePage(WebDriver driver) {
+		this.driver=driver;
+		PageFactory.initElements(driver, this);
+	}
+public void navigateTo() {
+	driver.get("http://localhost:4200/home");
+}
+	
+	
+}
