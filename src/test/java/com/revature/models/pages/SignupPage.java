13b790716e22b3e5e613db10b3d0143f1f1b44c2
Christian-Seefeldt <94253100+Christian-Seefeldt@users.noreply.github.com>
2022-02-15 14:14:16 -0500

    Add files via upload

---
+++ src/test/java/com/revature/models/pages/SignupPage.java
@@ -0,0 +1,21 @@
+package com.revature.models.pages;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class SignupPage {
+	
+	private  WebDriver driver;
+
+	public SignupPage(WebDriver driver) {
+		this.driver=driver;
+		PageFactory.initElements(driver, this);
+	}
+
+	public void navigateTo(String url) {
+		driver.get(url);
+	}	
+}
+	
